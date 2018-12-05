719bb4a452ca058d3ddb1ad600e879bfdc4d864a
nicholpt <36209110+nicholpt@users.noreply.github.com>
2018-12-05 03:38:21 -0800

    :/

--- django_project/settings.py
+++ django_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'psycopg2',
 
      # ...'whitenoise.runserver_nostatic',
 
