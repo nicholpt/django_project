2c42f3204969f30f89a22c324026fede27528de4
nicholpt <36209110+nicholpt@users.noreply.github.com>
2018-12-05 03:22:49 -0800

    stat

--- django_project/settings.py
+++ django_project/settings.py
@@ -139,8 +139,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
